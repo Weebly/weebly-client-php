f61aaa40d4cc224687f8793fc2b63e27b4d78093
Bryan Ashley <bryan@weebly.com>
2015-03-30 15:38:45 -0700

    Adds OAuth related public methods

--- src/Weebly/WeeblyClient.php
+++ src/Weebly/WeeblyClient.php
@@ -34,7 +34,7 @@ class WeeblyClient
 	 *
 	 * @return instance
 	 */
-	public function __construct($client_id, $client_secret, $user_id, $site_id=null)
+	public function __construct($client_id, $client_secret, $user_id, $site_id=null, $access_token=null)
 	{
 		$this->client_id = $client_id;
 		$this->client_secret = $client_secret;
@@ -42,4 +42,33 @@ class WeeblyClient
 		$this->site_id = $site_id;
 	}
 
+	/**
+	 * Returns the url to redirect to for oauth authentication (Step 1 in OAuth flow)
+	 *
+	 * @param (optional) array $scope           An array of the permissions your application is 
+	 *                                          requesting i.e (read:user, read:commerce)
+	 * @param (optional) string $redirect_uri   The url weebly will redirect to upon user's grant of 
+	 *                                          permissions. Defaults to application callback url
+	 *
+	 *
+	 * @return string $authorization_url
+	 */
+	public function getAuthorizationUrl($scope=array(), $redirect_uri=null)
+	{
+
+	}
+
+	/**
+	 * Exchanges a temporary authorization code for a permanent access_token
+	 *
+	 * @param string $authorization_code   The authorization_code sent from weebly after the user has 
+	 *                                     granted the application access to their data.
+	 *
+	 *
+	 * @return string $access_token
+	 */
+	public function getAccessToken($authorization_code)
+	{
+
+	}
 }
