962ba9a4ca9f424240efba82d94fa6fdf94e150b
Bryan Ashley <bryan@weebly.com>
2015-03-30 14:46:24 -0700

    Adds construct and initial properties

--- src/Weebly/WeeblyClient.php
+++ src/Weebly/WeeblyClient.php
@@ -4,5 +4,42 @@ namespace Weebly;
 
 class WeeblyClient 
 {
-		
+	/**
+	 * Weebly User Id
+	 */
+	public $user_id;
+
+	/**
+	 * Weebly Site Id
+	 */
+	public $site_id;
+
+	/**
+	 * Application Client Id
+	 */
+	private $client_id;
+
+	/**
+	 * Application Client Secret
+	 */
+	private $client_secret;
+
+	/**
+	 * Creates a new API interaction object.
+	 *
+	 * @param $client_id       Your application client_id
+	 * @param $client_secret   Your application client_secret
+	 * @param $user_id         The Weebly User Id
+	 * @param $site_id         The Weebly Site Id
+	 *
+	 * @return instance
+	 */
+	public function __construct($client_id, $client_secret, $user_id, $site_id=null)
+	{
+		$this->client_id = $client_id;
+		$this->client_secret = $client_secret;
+		$this->user_id = $user_id;
+		$this->site_id = $site_id;
+	}
+
 }
